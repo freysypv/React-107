303e919d56b8ac1b92bdbbba59f15fb3819f854a
freysypv <freysypv@hotmail.com>
2026-03-07 17:27:45 -0600

    first commit

---
+++ my-react-store/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Navbar from './components/Navbar';
+
+function App() {
+
+  return (
+   
+      <div>
+        <h1>Hello from react</h1>
+
+      </div>
+      
+   
+  )
+}
+
+export default App
---
+++ my-react-store/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+//imports
+import "./Navbar.css"
+
+//logic
+function Navbar() {
+    return (
+        <div>
+            <h1>My Online Store</h1>
+        </div>
+    )
+} 
+
+
+
+//exports
+export default Navbar;
